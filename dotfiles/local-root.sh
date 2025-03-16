9de7ae06fa511ff22abab78c7cd486b16ca62e5d
Thanadol Chomphoochan <tcpc@mit.edu>
2025-03-16 16:53:22 -0400

    Oh I guess it's actually called CPATH

--- dotfiles/local-root.sh
+++ dotfiles/local-root.sh
@@ -2,5 +2,5 @@
 export PATH="$HOME/.local/bin:$PATH"
 export LIBRARY_PATH="$HOME/.local/lib:$HOME/.local/lib64:$LIBRARY_PATH"
 export LD_LIBRARY_PATH="$HOME/.local/lib:$HOME/.local/lib64:$LD_LIBRARY_PATH"
-export INCLUDE_PATH="$HOME/.local/include:$INCLUDE_PATH"
+export CPATH="$HOME/.local/include:$CPATH"
 
