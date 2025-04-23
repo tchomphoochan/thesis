bba8286edbc5f5007aee87d087c38c03c5a62320
Thanadol Chomphoochan <tcpc@mit.edu>
2025-04-22 23:49:11 -0400

    Use loglog graph

--- model/test_bloom_filter.py
+++ model/test_bloom_filter.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python3
 
 import numpy as np
-import matplotlib.pyplot as plt
 import random
 import time
 import sys
 import itertools
+import tqdm
 
-from concurrent.futures import ProcessPoolExecutor
+import matplotlib.pyplot as plt
+from matplotlib.ticker import FuncFormatter
+
+from concurrent.futures import Executor, Future, ProcessPoolExecutor
 from typing import *
 
 from bloom_filter import Set, make_bloom_filter, make_parallel_bloom_filter
 
 def get_false_pos_rate(addr_space: list[int], num_elems: int, len_signature: int, num_partitions: int):
+  random.seed(1357924680)
   ft: Set = make_parallel_bloom_filter(len_signature, num_partitions)
 
   for i in range(num_elems):
@@ -30,54 +34,71 @@ def get_false_pos_rate(addr_space: list[int], num_elems: int, len_signature: int
   return false_pos_rate
 
 def get_data_for_config(len_signature: int, num_partitions: int) -> Tuple[np.array, np.array, str]:
-  max_log = 18
+  print(f"  m={len_signature}, k={num_partitions}", file=sys.stderr)
+  max_log = 20
   len_addr_space = 2**max_log
-  num_elems_arr = np.rint(2**np.arange(0, 12, 1.0/4.0)).astype(int)
+  num_elems_arr = np.rint(2**np.arange(0, 14, 1.0/4.0)).astype(int)
   addr_space = list(range(len_addr_space))
   random.shuffle(addr_space)
 
-  with ProcessPoolExecutor() as exec:
-    rates = list(exec.map(get_false_pos_rate,
-                          itertools.repeat(addr_space),
-                          num_elems_arr,
-                          itertools.repeat(len_signature),
-                          itertools.repeat(num_partitions)))
+  with tqdm.tqdm(total=len(num_elems_arr)) as progress:
+    with ProcessPoolExecutor() as pool:
+      futures = []
+      for num_elems in num_elems_arr:
+        future = pool.submit(get_false_pos_rate, addr_space, num_elems, len_signature, num_partitions)
+        future.add_done_callback(lambda _: progress.update())
+        futures.append(future)
+      rates = [future.result() for future in futures]
 
   rates = np.array(rates)
   return (num_elems_arr, rates, f'm={len_signature}, k={num_partitions}')
 
 def graph_false_pos_rate():
+  plt.rcParams['axes.formatter.min_exponent'] = 20
+
   config_arrs = [
-    # same m/k
-    [(128, 1), (256, 2), (512, 4), (1024, 8)],
+    # same m/k (number of bits overall increase)
+    [(512, 1), (1024, 2), (2048, 4), (4096, 8), (8192, 16)],
     # same k, increasing m
-    [(128, 4), (256, 4), (512, 4), (1024, 4)],
+    [(512, 4), (1024, 4), (2048, 4), (4096, 4), (8192, 4)],
     # same m, increasing k
     [(1024, 1), (1024, 2), (1024, 4), (1024, 8)],
-    # common configs
-    [(512, 4), (512, 8), (1024, 4), (1024, 8)],
+    # same as above
+    [(4096, 1), (4096, 2), (4096, 4), (4096, 8)],
+    None,
   ]
 
   for index, config_arr in enumerate(config_arrs):
+    filename = f"output-bloom-filter-{index}.svg"
+    action = "Rendering" if config_arr else "Skipping"
+    print(f"{action}: {filename}", file=sys.stderr)
+    if not config_arr:
+      continue
+
     begin = time.time()
 
-    plt.figure()
-    plt.title("False positive rate of parallel bloom filters")
-    plt.xlabel("Number of elements inserted ($n$)")
-    plt.ylabel("False positive rate")
-    plt.xscale('log', base=2)
-    plt.yscale('linear')
-    plt.grid()
+    fig, ax = plt.subplots()
+    ax.set_title("False positive rate of parallel bloom filters")
+
+    ax.set_xlabel("Number of elements inserted ($n$)")
+    ax.set_ylabel("False positive rate")
+    ax.set_xscale('log', base=2)
+    ax.set_yscale('log', base=10)
+    ax.set_ylim(1e-4-3e-5, 1+0.3)
+    ax.grid()
+
+    # for axis in [ax.xaxis, ax.yaxis]:
+    #   formatter = FuncFormatter(lambda y, _: '{:.8g}'.format(y))
+    #  axis.set_major_formatter(formatter)
 
     for x, y, label in map(get_data_for_config, *zip(*config_arr)):
-      plt.plot(x, y, '-', label=label)
+      ax.plot(x, y, '-', label=label)
 
-    plt.legend()
+    ax.legend()
     end = time.time()
 
-    filename = f"output-bloom-filter-{index}.svg"
-    print(f"Took {end-begin} seconds to render {filename}", file=sys.stderr)
-    plt.savefig(filename)
+    print(f"Done: {filename}, {end-begin}", file=sys.stderr)
+    fig.savefig(filename)
 
 if __name__ == "__main__":
   graph_false_pos_rate()
