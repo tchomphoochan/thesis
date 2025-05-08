441e7582a26b705f43faff29be9783ee90169359
Thanadol Chomphoochan <t.chomphoochan@gmail.com>
2025-05-07 21:42:25 -0400

    Full analyze draft

--- runner/src/analyze.c
+++ runner/src/analyze.c
@@ -11,16 +11,21 @@
 #include "pmutils.h"
 #include "workload.h"
 
-static bool complain_missing  = false;
-static bool complain_order    = true;
-static bool complain_conflict = true;
+static const bool complain_missing  = false;
+static const bool complain_order    = true;
+static const bool complain_conflict = true;
+
+static const int num_buckets = 64;
+static const double fraction_warmup_time = 0.1;
+static const double fraction_cooldown_time = 0.1;
+static const int num_throughput_windows = 100;
 
 typedef struct {
-  uint64_t submit, sched, work, done, cleanup;   /* 0 if missing       */
-  uint16_t puppet;                             /* from DONE          */
+  uint64_t submit, sched, work, done, cleanup; // 0 is missing
+  bool complete;
+  bool ordered;
+  uint16_t puppet;
 } timeline_t;
-
-static inline double cycles_to_us(uint64_t c) { return c / cpu_freq * 1e6; }
 
 typedef struct { uint64_t ts_sched, ts_done; int id; } sched_evt_t;
 
@@ -39,7 +44,9 @@ int main(int argc, char *argv[])
     exit(1);
   }
 
-  /* Load all data */
+  /*
+  Load all data
+  */
 
   const char *csv_file  = argv[1];
   const char *log_file  = argv[2];
@@ -60,9 +67,11 @@ int main(int argc, char *argv[])
   INFO("Loaded %d transactions, %d log events, cpu_freq=%.3f GHz",
        wl->num_txns, num_events, cpu_freq / 1e9);
 
-  /* Construct transaction timelines */
+  /*
+  Construct transaction timelines
+  */
 
-  timeline_t *tl = (timeline_t *) calloc(wl->num_txns, sizeof(*tl));
+  timeline_t *tl = (timeline_t *) calloc(wl->num_txns, sizeof(timeline_t));
   ASSERT(tl);
 
   uint64_t first_submit = UINT64_MAX, last_done = 0;
@@ -79,9 +88,12 @@ int main(int argc, char *argv[])
     }
   }
 
-  /* Record first_submit and last_done. Also complain about incomplete transactions.  */
+  /*
+  Record first_submit and last_done. Also complain about incomplete transactions.
+  */
 
   int missing = 0, order_err = 0;
+  int complete_txns = 0, ordered_txns = 0;
   for (int i = 0; i < wl->num_txns; i++) {
     if (!tl[i].submit || !tl[i].sched || !tl[i].work || !tl[i].done || !tl[i].cleanup) {
       if (complain_missing) {
@@ -91,6 +103,8 @@ int main(int argc, char *argv[])
       missing++;
       continue;
     }
+    tl[i].complete = true;
+    complete_txns++;
 
     if (!(tl[i].submit <= tl[i].sched && tl[i].sched <= tl[i].work
       && tl[i].work <= tl[i].done && tl[i].done <= tl[i].cleanup)) {
@@ -99,6 +113,9 @@ int main(int argc, char *argv[])
         if (order_err == 10) INFO("Further ordering violations will be omitted.");
       }
       order_err++;
+    } else {
+      tl[i].ordered = true;
+      ordered_txns++;
     }
 
     if (tl[i].submit < first_submit) first_submit = tl[i].submit;
@@ -120,6 +137,10 @@ int main(int argc, char *argv[])
   } else {
     INFO("All available transactions are correctly ordered.", wl->num_txns);
   }
+
+  /*
+  Conflict checks
+  */
 
   if (!complain_conflict) {
     // Very expensive operation. If we aren't gonna complain, just skip entirely.
@@ -165,9 +186,7 @@ int main(int argc, char *argv[])
 
         if (check_txn_conflict(cur_tx, &wl->txns[other_id])) {
           if (conflicts < 10) {
-            WARN("Conflict: txn %d vs %d (sched %.3f µs)",
-                 cur_id, other_id,
-                 cycles_to_us(now_sched - base_tsc));
+            WARN("Conflict: txn %d vs %d", cur_id, other_id);
           }
           if (conflicts == 10) {
             WARN("Further conflicts omitted");
@@ -177,7 +196,7 @@ int main(int argc, char *argv[])
         }
       }
 
-      /* add current txn to active set */
+      // Add current txn to active set
       if (active_cnt < num_puppets)
         active_ids[active_cnt++] = cur_id;
     }
@@ -191,11 +210,104 @@ int main(int argc, char *argv[])
     free(sched);
   }
 
+  // binary file format: start
+  // 
+  // - int: total number of transactions
+  // - int: number of logged transactions
+  // - int: number of histogram-included transactions (important for histogram y axis)
+  // - int: num puppets
+  // - double: extrapolated overall throughput txn/s (with warmup/cooldown removed)
+  //
+  // windowed throughput. all logged transactions are used for calculation (n = logged). extrapolate to total txns.
+  // - int: number of throughput windows
+  // - double: throughput window size in seconds (to print it on the graph)
+  // - n pairs of (64-bit double in seconds x, 64-bit double throughput/s y) for submit
+  // - n pairs of (64-bit double in seconds x, 64-bit double throughput/s y) for ready
+  // - n pairs of (64-bit double in seconds x, 64-bit double throughput/s y) for work recv
+  // - n pairs of (64-bit double in seconds x, 64-bit double throughput/s y) for done
+  // - n pairs of (64-bit double in seconds x, 64-bit double throughput/s y) for cleanup
+  //
+  // end-to-end latency data. all included transactions are used for histogram. (n = included)
+  // - int: num histogram buckets
+  // - pairs of (64-bit double in seconds for histogram center x, int: count y, double: CDF y2)
+  //    - n for e2e (submit->done)
+  //    - n for submit->sched
+  //    - n for sched->work
+  //    - n for work->done
+  //    - n for done->cleanup
+  //
+  // binary file format: end
+
+  FILE *out = fopen("out.bin", "wb");
+
+  /*
+  Compute windowed throughput (all completely logged transactions)
+  */
+  uint64_t duration_cycles = last_done - first_submit;
+  uint64_t window_cycles = duration_cycles / num_throughput_windows;
+  // counters
+  int *windowed_submits  = (int*) calloc(num_throughput_windows, sizeof(int));
+  int *windowed_scheds   = (int*) calloc(num_throughput_windows, sizeof(int));
+  int *windowed_recvs    = (int*) calloc(num_throughput_windows, sizeof(int));
+  int *windowed_dones    = (int*) calloc(num_throughput_windows, sizeof(int));
+  int *windowed_cleanups = (int*) calloc(num_throughput_windows, sizeof(int));
+
+  // TODO: go through tl with completed, increment window
+
+
+  /*
+  Compute latencies (all complete and ordered transactions within time range of interest).
+  */
+
+  // range of interest:
+  // a transaction is included if submit >= first_included and done <= last_included.
+  uint64_t first_included = first_submit + duration_cycles*fraction_warmup_time;
+  uint64_t last_included = last_done - duration_cycles*fraction_cooldown_time;
+
+  int latency_count = 0;
+  uint64_t *submit_done_latencies = (uint64_t*) calloc(wl->num_txns, sizeof(uint64_t));
+  uint64_t *submit_sched_latencies = (uint64_t*) calloc(wl->num_txns, sizeof(uint64_t));
+  uint64_t *sched_recv_latencies   = (uint64_t*) calloc(wl->num_txns, sizeof(uint64_t));
+  uint64_t *recv_done_latencies    = (uint64_t*) calloc(wl->num_txns, sizeof(uint64_t));
+  uint64_t *done_cleanup_latencies = (uint64_t*) calloc(wl->num_txns, sizeof(uint64_t));
+
+  // TODO: for ordered_txns included in the range, add to latency data
+  // TODO: also figure out how to calculate average throughput within range of interest
+  //       (maybe count num of txns in range, multiply by extrapolate, divided by duration?)
+
+
+  /*
+  Output binary file for graphing
+  */
+
+  // throughput extrapolation
+  double extrapolate_factor = (double) wl->num_txns / complete_txns;
+  double average_throughput; // TODO
+
+  fwrite(&wl->num_txns, sizeof(int), 1, out);
+  fwrite(&complete_txns, sizeof(int), 1, out);
+  fwrite(&latency_count, sizeof(int), 1, out);
+  fwrite(&num_puppets, sizeof(int), 1, out);
+  fwrite(&average_throughput, sizeof(int), 1, out);
+
+  // output windowed throughputs
+  fwrite(&num_throughput_windows, sizeof(int), 1, out);
+  double window_seconds; // TODO
+  fwrite(&window_seconds, sizeof(double), 1, out);
+  // TODO: use the counts above, but recalculate as txn/s. also recalculate timestamp cycles into seconds. output to file.
+  // output for each kind of throughput
+
+
+  // TODO: output latency histogram+cdf
+
+
+  fclose(out);
+
   /*
   Print estimated throughputs
   */
-  double elapsed_s = (last_done - first_submit) / cpu_freq;
-  double throughput = (double)wl->num_txns / elapsed_s;
+  double elapsed_s = (last_done - first_submit) / cpu_freq; // clearly mark as wallclock time
+  double throughput = (double)wl->num_txns / elapsed_s; // TODO: use the better calculated version
 
   printf("Summary\n"
          "========\n"
